b5c12db8ff89f04177d93568dac3333ed78c1725
David Moya <3nueves@gmail.com>
2023-10-29 19:43:05 +0100

    update script

--- users/add-user.sh
+++ users/add-user.sh
@@ -2,24 +2,27 @@
 # invisiblebits.com
 # v1.0 by David Moya
 
-echo -e "Create user kub\n"
-adduser --home /home/kub --shell /bin/bash --uid 1001 --disabled-password --gecos GECOS kub
+USER=$1
+uid=$2
+
+echo -e "Create user $USER with uid $uid\n"
+adduser --home /home/$USER --shell /bin/bash --uid $uid --disabled-password --gecos GECOS $USER
 
 echo -e "Change password\n"
-echo -en "changeme01\nchangeme01\nchangeme01\n" | passwd kub
+echo -en "changeme01\nchangeme01\nchangeme01\n" | passwd $USER
 
 echo -e "Enable access from ssh keys\n"
-mkdir -p /home/kub/.ssh/
-touch /home/kub/.ssh/authorized_keys
-cat $HOME/.ikctl/id_rsa_kubernetes.pub >> /home/kub/.ssh/authorized_keys
-chmod 700 /home/kub/.ssh
-chmod 600 /home/kub/.ssh/authorized_keys
-chown 1000.1000 /home/kub -R
+mkdir -p /home/$USER/.ssh/
+touch /home/$USER/.ssh/authorized_keys
+cat $HOME/.ikctl/id_rsa_kubernetes-unelink.pub >> /home/$USER/.ssh/authorized_keys
+chmod 700 /home/$USER/.ssh
+chmod 600 /home/$USER/.ssh/authorized_keys
+chown 1000.1000 /home/$USER -R
 
 echo -e "Add nopasswd in sodoers group\n"
-echo 'kub ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/kub
-chmod 440 /etc/sudoers.d/kub
-usermod -aG sudo kub
+echo -e "${USER} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/$USER
+chmod 440 /etc/sudoers.d/$USER
+usermod -aG sudo $USER
 
 echo -e "Change keywoard to ES\n"
 sed -i "s/pc105/pc104/g" /etc/default/keyboard
