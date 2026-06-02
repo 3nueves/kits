085a871cdc0a3b8c4b5a520cb69384b68d493021
3nueves <3nueves@gmail.com>
2026-06-03 00:00:09 +0200

    Update create-users kit to enhance user creation process with SSH key support and improved configuration

--- create-users/main.sh
+++ create-users/main.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-source .ikctl/users.sh
+set -e
 
-export users=$*
-export pass="changeme01"
+USER="{{ user }}"
+PASS="{{ pass }}"
 
-for usr in ${users[@]}
-do
-    export user=$usr
-    value=$(check_user)
+if getent passwd "$USER" > /dev/null 2>&1; then
+    echo "$USER already exists"
+    exit 0
+fi
 
-    if [ ${value} = "not_exist" ]; then
-        add_user
-        add_ssh_key
-        add_sudoers
-    else
-        echo -e "$user already exist\n"
-    fi
+echo "Creating user $USER..."
+adduser --home /home/$USER --shell /bin/bash --disabled-password --gecos "" $USER
 
-done
+echo "Setting password for $USER..."
+echo "$USER:$PASS" | chpasswd
+
+echo "User $USER created successfully"
+
+{% if ssh_public_key %}
+echo "Configuring SSH key for $USER..."
+mkdir -p /home/$USER/.ssh
+echo "{{ ssh_public_key }}" >> /home/$USER/.ssh/authorized_keys
+chmod 700 /home/$USER/.ssh
+chmod 600 /home/$USER/.ssh/authorized_keys
+chown -R $USER:$USER /home/$USER/.ssh
+echo "SSH key configured"
+{% endif %}
